566f219c5d1fbbc41dcb70c546664821676aec1e
fanrell <cieslik.kamil@outlook.com>
2020-11-12 19:12:56 +0100

    Sceens and switches between them

---
+++ Assets/Scripts/ButtonActions/MainMenuActions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuActions : MonoBehaviour
+{
+    public void Close()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GoToProfiles()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToCharacterSheetList()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
